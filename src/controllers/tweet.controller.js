feb83fd45e87c6930c7416e38088ad4fb391401c
Abhishek Singh <8090singhabhi@gmail.com>
2025-02-28 23:46:58 +0530

    tweet route completed

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -6,19 +6,91 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
-    //TODO: create tweet
+    const { tweetContent } = req.body
+    if (!tweetContent) {
+        throw new ApiError(400, "tweetContent is not found - createTweet")
+    }
+    
+    const userId = req.user?._id
+    if (!userId) {
+        throw new ApiError(400, "userId is not found - createTweet")
+    }
+
+    const tweet = await Tweet.create({
+        content: tweetContent,
+        owner: userId
+    })
+
+    if (!tweet) {
+        throw new ApiError(500, "something went wrong while - createTweet")
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200, tweet, "tweet created successfully")
+    )
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
-    // TODO: get user tweets
+    const { userId } = req.params // .route("/user/:userId")
+    const userTweets = await Tweet.find(
+        {owner: userId}
+    )
+
+    if (!userTweets) {
+        throw new ApiError(500, "userTweets not found - getUserTweets")
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200, userTweets, "userTweets fetched successfully")
+    )
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
-    //TODO: update tweet
+    const { tweetId } = req.params
+    if(!isValidObjectId(tweetId)){
+        throw new ApiError(400, "tweetId is not found - updateTweet")
+    }
+
+    const { updatedContent } = req.body
+    if(!updatedContent){
+        throw new ApiError(400, "updatedContent is not found - updateTweet")
+    }
+
+    const updatedTweet = await Tweet.findByIdAndUpdate(
+        tweetId,
+        {
+            $set: {
+                content: updatedContent
+            }
+        },
+        { new: true }
+    )
+
+    if(!updatedTweet){
+        throw new ApiError(400, "updatedTweet is not found - updateTweet")
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200, updatedTweet, "tweet updated successfully")
+    )
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
-    //TODO: delete tweet
+    const { tweetId } = req.params
+    if(!isValidObjectId(tweetId)){
+        throw new ApiError(400, "tweetId is not found - updateTweet")
+    }
+
+    const response = await Tweet.findByIdAndDelete(tweetId)
+    if(!response){
+        throw new ApiError(400, "error while deleting video")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, "Tweet deleted Successfully")
+    )
 })
 
 export {
--- src/routes/tweet.route.js
+++ src/routes/tweet.route.js
@@ -10,8 +10,17 @@ import {verifyJWT} from "../middlewares/auth.middleware.js"
 const router = Router();
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-router.route("/").post(createTweet);
-router.route("/user/:userId").get(getUserTweets);
-router.route("/:tweetId").patch(updateTweet).delete(deleteTweet);
+router
+    .route("/")
+    .post(createTweet);
+
+router
+    .route("/user/:userId")
+    .get(getUserTweets);
+
+router
+    .route("/:tweetId")
+    .patch(updateTweet)
+    .delete(deleteTweet);
 
 export default router
