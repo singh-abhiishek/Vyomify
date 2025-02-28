9e412e01a6817f9cd98d2d03055b1e87affb9eac
Abhishek Singh <8090singhabhi@gmail.com>
2025-02-28 23:52:15 +0530

    like template added

--- src/app.js
+++ src/app.js
@@ -29,6 +29,7 @@ import userRouter from "./routes/user.route.js"
 import videoRouter from "./routes/video.route.js"
 import commentRouter from "./routes/comment.route.js"
 import tweetRouter from "./routes/tweet.route.js"
+import likeRouter from "./routes/like.route.js"
 
 // routes declaration
 app.use("/api/v1/healthcheck", healthRouter)
@@ -36,6 +37,7 @@ app.use("/api/v1/users", userRouter)
 app.use("/api/v1/videos", videoRouter)
 app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/likes", likeRouter)
 
 
 
