67e7e711c3c255073d799f8a666f149f548640f4
Abhishek Singh <8090singhabhi@gmail.com>
2025-03-05 23:55:32 +0530

    subscription template added

--- src/app.js
+++ src/app.js
@@ -31,6 +31,7 @@ import commentRouter from "./routes/comment.route.js"
 import tweetRouter from "./routes/tweet.route.js"
 import likeRouter from "./routes/like.route.js"
 import playlistRouter from "./routes/playlist.route.js"
+import subscriptionRouter from "./routes/subscription.route.js"
 
 // routes declaration
 app.use("/api/v1/healthcheck", healthRouter)
@@ -40,6 +41,7 @@ app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/tweets", tweetRouter)
 app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/playlists", playlistRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
 
 
 // http://localhost:8000/api/v1/users/register
