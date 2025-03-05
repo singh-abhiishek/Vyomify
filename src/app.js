b35f02384d5230a57fb9a6165045449ff0e77fa3
Abhishek Singh <8090singhabhi@gmail.com>
2025-03-05 19:21:47 +0530

    playlist template added

--- src/app.js
+++ src/app.js
@@ -16,8 +16,8 @@ app.use(cors({
     // Example: Login system where the backend sets a cookie for authentication.
 }))
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
 
@@ -30,6 +30,7 @@ import videoRouter from "./routes/video.route.js"
 import commentRouter from "./routes/comment.route.js"
 import tweetRouter from "./routes/tweet.route.js"
 import likeRouter from "./routes/like.route.js"
+import playlistRouter from "./routes/playlist.route.js"
 
 // routes declaration
 app.use("/api/v1/healthcheck", healthRouter)
@@ -38,7 +39,7 @@ app.use("/api/v1/videos", videoRouter)
 app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/tweets", tweetRouter)
 app.use("/api/v1/likes", likeRouter)
-
+app.use("/api/v1/playlists", playlistRouter)
 
 
 // http://localhost:8000/api/v1/users/register
