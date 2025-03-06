e8d308683f070bff8d2502aee979ecfce2177c6f
Abhishek Singh <8090singhabhi@gmail.com>
2025-03-06 17:05:21 +0530

    subscription controller done

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -8,17 +8,66 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
-    // TODO: toggle subscription
+    if (!isValidObjectId(channelId)) {
+        throw new ApiError(400, "Invalid channelId - toggleSubscription")
+    }
+
+    const userId = req.user?._id
+    if (!userId) {
+        throw new ApiError(400, "Invalid userId - toggleSubscription")
+    }
+
+    const isSubscribed = await Subscription.findOne(
+       {
+        subscriber: userId,
+        channel: channelId
+       }
+    )
+
+    if (isSubscribed) {
+        const response = await Subscription.findByIdAndDelete(isSubscribed?._id)
+        if (!response) {
+            throw new ApiError(400, "error while unsubscribing channel - toggleSubscription")
+        }
+        return res.status(200).json(new ApiResponse(200, "channel Unsubscribed Successfully"))
+    }else{
+        const response = await Subscription.create({
+            subscriber: userId,
+            channel: channelId  
+        })
+        if (!response) {
+            throw new ApiError(400, "error while subscribing channel - toggleSubscription")
+        }
+        return res.status(200).json(new ApiResponse(200, response, "channel Subscribed Successfully"))
+    }
 })
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
+    const { channelId } = req.params
+    if (!isValidObjectId(channelId)) {
+        throw new ApiError(400, "Invalid channelId - getUserChannelSubscribers")
+    }
+
+    const subscribers = await Subscription.countDocuments({ channel: channelId })
+    if (!subscribers) {
+        throw new ApiError(400, "error while counting subscribers")
+    }
+    return res.status(200).json(new ApiResponse(200, subscribers, "user subscribers fetched successfully"))
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
     const { subscriberId } = req.params
+    if (!isValidObjectId(subscriberId)) {
+        throw new ApiError(400, "Invalid subscriberId - getSubscribedChannels")
+    }
+
+    const subscribedTo = await Subscription.countDocuments({ subscriber: subscriberId })
+    if (!subscribedTo) {
+        throw new ApiError(400, "error while counting subscribedTo")
+    }
+    return res.status(200).json(new ApiResponse(200, subscribedTo, "user subscribedTo channel fetched successfully"))
 })
 
 export {
