e48d1ebe7f6e3eed1e966a08da3b149be07de9ea
singh-abhiishek <8090singhabhi@gmail.com>
2025-04-21 16:06:46 +0530

    add to watch later,remove, get video from watchlater controller added

--- backend/src/controllers/playlist.controller.js
+++ backend/src/controllers/playlist.controller.js
@@ -16,11 +16,21 @@ const createPlaylist = asyncHandler(async (req, res) => {
         [name, description].some((field) =>
             field?.trim() === "")
     ) {
-        throw new ApiError(410, "All fields are required - createPlaylist")
+        throw new ApiError(400, "All fields are required - createPlaylist")
     }
+
     const userId = req.user?._id
     if (!isValidObjectId(userId)) {
-        throw new ApiError(410, "Invalid userId - createPlaylist")
+        throw new ApiError(401, "Invalid userId - createPlaylist")
+    }
+
+    const isSameNamePlaylistExist = await Playlist.findOne({
+        owner: new mongoose.Types.ObjectId(userId),
+        name: name
+    })
+
+    if (isSameNamePlaylistExist) {
+        throw new ApiError(400, "playlist with this name already exist - createPlaylist")
     }
 
     const createdPlaylist = await Playlist.create({
@@ -91,11 +101,11 @@ const addVideoToPlaylist = asyncHandler(async (req, res) => {
     if (!videoId) {
         return next(new ApiError(400, "videoId is missing - addVideoToPlaylist"));
     }
-    
+
     if (!isValidObjectId(videoId)) {
         throw new ApiError(400, "Invalid videoId - addVideoToPlaylist")
     }
-    
+
     let playlistIds = [];
     playlistIds = (req.body.playlistIds || "[]");
 
@@ -167,10 +177,6 @@ const getUserPlaylists = asyncHandler(async (req, res) => {
         owner: userId
     })
 
-    if (!userPlaylists) {
-        throw new ApiError(400, "error while fetching user playlists - getUserPlaylists")
-    }
-
     if (userPlaylists.length === 0) {
         return res.status(200).json(new ApiResponse(200, "No playlists found"))
     }
@@ -182,6 +188,7 @@ const getUserPlaylists = asyncHandler(async (req, res) => {
 
 const getUserPlaylistsName = asyncHandler(async (req, res) => {
     const { userId } = req.params
+    console.log(userId)
     if (!isValidObjectId(userId)) {
         throw new ApiError(400, "Invalid userId - getUserPlaylists")
     }
@@ -221,6 +228,196 @@ const getUserPlaylistsName = asyncHandler(async (req, res) => {
     )
 })
 
+const addToWatchLater = asyncHandler(async (req, res) => {
+    const { videoId } = req.params
+
+    if (!videoId) {
+        throw new ApiError(400, "videoId is missing - getUserPlaylists")
+    }
+
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid videoId - getUserPlaylists")
+    }
+
+    const userId = req?.user?._id;
+
+    const addVideoToWatchLater = await Playlist.findOneAndUpdate(
+        {
+
+            owner: new mongoose.Types.ObjectId(userId),
+            name: "Watch Later",
+            isWatchLater: true,
+
+        },
+        {
+            $addToSet: { videos: videoId } // use $push if duplicates are allowed
+        },
+        {
+            new: true
+        }
+    )
+
+    return res.status(200).json(
+        new ApiResponse(200, "video added to watch later successfully")
+    )
+})
+
+const removeFromWatchLater = asyncHandler(async (req, res) => {
+    const { videoId } = req.params
+
+    if (!videoId) {
+        throw new ApiError(400, "videoId is missing - getUserPlaylists")
+    }
+
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid videoId - getUserPlaylists")
+    }
+
+    const userId = req?.user?._id;
+
+    await Playlist.findOneAndUpdate(
+        {
+
+            owner: new mongoose.Types.ObjectId(userId),
+            name: "Watch Later",
+            isWatchLater: true,
+
+        },
+        {
+            $pull: { videos: videoId } // use $push if duplicates are allowed
+        },
+        {
+            new: true
+        }
+    )
+
+    return res.status(200).json(
+        new ApiResponse(200, "video removed from watch later successfully")
+    )
+})
+
+const isVideoAlreadyInWatchLater = asyncHandler(async (req, res) => {
+    const { videoId } = req.params
+
+    if (!videoId) {
+        throw new ApiError(400, "videoId is missing - getUserPlaylists")
+    }
+
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid videoId - getUserPlaylists")
+    }
+
+    const userId = req?.user?._id;
+
+    const isAdded = await Playlist.findOne(
+        {
+
+            owner: new mongoose.Types.ObjectId(userId),
+            name: "Watch Later",
+            isWatchLater: true,
+            videos: new mongoose.Types.ObjectId(videoId) // this checks inside the array
+        }
+    )
+
+    console.log(isAdded)
+
+    if (isAdded) {
+        return res.status(200).json(
+            new ApiResponse(200, isAdded, "video already added in watch later")
+        )
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200, false, "video is not already added in watch later")
+    )
+})
+
+const getAllWatchLaterVideos = asyncHandler(async (req, res) => {
+    const userId = req.user?._id;
+
+    if (!userId) {
+        throw new ApiError(400, "userId is missing", getAllWatchLaterVideos)
+    }
+
+    const WatchLaterVideosList = await Playlist.aggregate([
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId(userId),
+                name: "Watch Later",
+                isWatchLater: true,
+                videos: { $exists: true },
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "videos",
+                foreignField: "_id",
+                as: "video",
+                pipeline: [
+                    {
+                        $project: {
+                            videoFile: 1,
+                            thumbnail: 1,
+                            title: 1,
+                            description: 1,
+                            duration: 1,
+                            views: 1,
+                            isPublished: 1,
+                            ownerUsername: 1,
+                            owner: 1,
+                            createdAt: 1
+                        }
+                    },
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "ownerDetails",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        _id: 1,
+                                        avatar: 1,
+                                        fullName: 1,
+                                        username: 1,
+                                        createdAt: 1,
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            ownerDetails: {
+                                $first: "$ownerDetails"
+                            }
+                        }
+                    },
+                    {
+                        $project: {
+                            _id: 0,
+                            ownerUsername: 0,
+                            owner: 0,
+                            // createdAt: 0
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $project: {
+                videos: 0
+            }
+        }
+    ])
+
+    return res.status(200).json(
+        new ApiResponse(200, WatchLaterVideosList, "all watch later videos fetced successfully")
+    )
+})
+
 export {
     createPlaylist,
     getPlaylistById,
@@ -229,5 +426,9 @@ export {
     deletePlaylist,
     updatePlaylist,
     getUserPlaylists,
-    getUserPlaylistsName
+    getUserPlaylistsName,
+    addToWatchLater,
+    removeFromWatchLater,
+    isVideoAlreadyInWatchLater,
+    getAllWatchLaterVideos
 }
