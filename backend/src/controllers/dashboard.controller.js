f863786913a88cc55e16e48df1761622270940ac
singh-abhiishek <8090singhabhi@gmail.com>
2025-04-22 23:33:44 +0530

    dashboard controller updated

--- backend/src/controllers/dashboard.controller.js
+++ backend/src/controllers/dashboard.controller.js
@@ -1,69 +1,113 @@
-import mongoose, { Aggregate } from "mongoose"
-import {Video} from "../models/video.model.js"
-import {Subscription} from "../models/subscription.model.js"
-import {Like} from "../models/like.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose, { Aggregate, isValidObjectId } from "mongoose"
+import { Video } from "../models/video.model.js"
+import { Subscription } from "../models/subscription.model.js"
+import { Like } from "../models/like.model.js"
+import { Tweet } from "../models/tweet.model.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // Get the channel stats like total video views, total subscribers, total videos, total likes etc.
     const userId = req.user?._id
-    if(!userId) {
+    if (!userId) {
         throw new ApiError(400, "userId is not found - getChannelStats")
     }
 
-    const totalSubscribers = await Subscription.countDocuments({ channel: userId })
-    if (!totalSubscribers) {
-        throw new ApiError(400, "error while counting subscribers")
-    }
-
-    const totalLikes = await Like.countDocuments({ postOwner: userId })
-    if (!totalLikes) {
-        throw new ApiError(400, "error while counting totalLikes")
-    }
+    const countSubscribers = await Subscription.countDocuments({ channel: userId })
+    const countSubscribedTo = await Subscription.countDocuments({ subscriber: userId })
+    const countLikes = await Like.countDocuments({ postOwner: userId })
+    const countTweets = await Tweet.countDocuments({ owner: userId })
 
     const response = await Video.aggregate([
-        { $match: { owner: new mongoose.Types.ObjectId(userId) } },
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId(userId)
+            }
+        },
         {
             $group: {
-                _id: userId,
-                totalViews: { $sum: "$views" },
-                totalVideos: { $sum: 1 }
+                _id: "$owner", // group by owner
+                countViews: { $sum: "$views" },
+                countVideos: { $sum: 1 }
+            }
+        },
+        {
+            $project: {
+                _id: 0,
             }
         }
     ])
 
-    if (!response) {
-        throw new ApiError(400, "error while calculating views and videos")
-    }
-    const totalViews = response[0].totalViews
-    const totalVideos = response[0].totalVideos
+    const countViews = response[0]?.countViews || 0;
+    const countVideos = response[0]?.countVideos || 0;
 
     return res.status(200).json(
-        new ApiResponse(200, { totalSubscribers, totalLikes, totalViews, totalVideos }, "channel stats fetched successfully")
+        new ApiResponse(
+            200,
+            {
+                countSubscribers,
+                countSubscribedTo,
+                countLikes,
+                countTweets,
+                countViews,
+                countVideos
+            },
+            "channel stats fetched successfully")
     )
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
-    // Get all the videos uploaded by the channel
     const userId = req.user?._id
-    if(!userId) {
-        throw new ApiError(400, "userId is not found - getChannelVideos")
+
+    if (!userId) {
+        throw new ApiError(400, "userId is missing - getChannelVideos")
     }
 
-    const allVideos = await Video.find(
-        { owner: userId}
-    ).select("-_id -owner -createdAt -updatedAt")
-
-    if(!allVideos) {
-        throw new ApiError(400, "error while fetching all videos of channel - getChannelVideos")
+    if (!isValidObjectId(userId)) {
+        throw new ApiError(400, "Invalid userId - getChannelVideos")
     }
-    console.log(allVideos)
-    return res.status(200).json(new ApiResponse(200, allVideos, "all channel videos fetched successfully"))
+
+    // aggregrate
+    const allVideos = await Video.aggregate([
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId(userId)
+            }
+        },
+        {
+            $project: {
+                owner: 0,
+                ownerUsername: 0,
+                videoFile: 0,
+            }
+        },
+        {
+            $lookup: {
+                from: "likes",
+                localField: "_id",
+                foreignField: "video",
+                as: "countLikes",
+                pipeline: [
+                    {
+                        $count: "countLikes"
+                    },
+                ]
+            }
+        },
+        {
+            $addFields: {
+                countLikes: { $ifNull: [{ $first: "$countLikes.countLikes" }, 0] }
+            }
+        }
+    ])
+
+    // console.log(allVideos)
+    return res.status(200).json(new ApiResponse(200, allVideos, "all videos of channel fetched successfully"))
 })
 
 export {
-    getChannelStats, 
+    getChannelStats,
     getChannelVideos
 }
