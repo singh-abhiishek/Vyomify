0a6df28b6c61da499708b1e51d337add16532343
singh-abhiishek <8090singhabhi@gmail.com>
2025-05-02 22:08:42 +0530

    IsVideoAlreadyInWatchLater, getAllWatchLaterVideos and more endpoints added

--- frontend/src/store/slices/playlistApiSlice.js
+++ frontend/src/store/slices/playlistApiSlice.js
@@ -14,24 +14,58 @@ export const plalylistApiSlice = apiSlice.injectEndpoints({
                 method: "POST",
                 body: data,
             }),
+            
         }),
 
-        // add video to playlist add/:videoId
+        // update playlist
+        updatePlaylist: builder.mutation({
+            query: ({playlistId,... data}) => ({
+                url: `${PLAYLISTS_URL}/${playlistId}`,
+                method: "PATCH",
+                body: data,
+            }),
+            invalidatesTags: ["DeletePlaylist"],
+        }),
+
+        // delete playlist
+        deletePlaylist: builder.mutation({
+            query: (playlistId) => ({
+                url: `${PLAYLISTS_URL}/${playlistId}`,
+                method: "DELETE",
+            }),
+            invalidatesTags: ["DeletePlaylist"],
+        }),
+
+        // add video to playlist 
         addVideoToPlaylists: builder.mutation({
-            query: ({videoId, ...playlistIds}) => ({
+            query: ({ videoId, ...playlistIds }) => ({
                 url: `${PLAYLISTS_URL}/add/${videoId}`,
                 method: "PATCH",
                 body: playlistIds,
             }),
         }),
 
-        // delete playlist
-
+        // remove video from playlist
+        removeVideoFromPlaylist: builder.mutation({
+            query: ({ videoId, playlistId }) => ({
+                url: `${PLAYLISTS_URL}/remove/${videoId}/${playlistId}`,
+                method: "PATCH",
+            }),
+        }),
 
         // get users playlists
         getUserPlaylists: builder.query({
             query: (userId) => ({
-                url: `${PLAYLISTS_URL}/user/${userId}`
+                url: `${PLAYLISTS_URL}/user/${userId}/playlists`
+            }),
+            providesTags: ["DeletePlaylist"]
+        }),
+
+        // get playlist by playlist Id
+        getPlaylistById: builder.query({
+            query: (playlistId) => ({
+                url: `${PLAYLISTS_URL}/${playlistId}`,
+                method: "GET"
             }),
         }),
 
@@ -48,6 +82,7 @@ export const plalylistApiSlice = apiSlice.injectEndpoints({
                 url: `${PLAYLISTS_URL}/add/watchLater/${videoId}`,
                 method: "PATCH"
             }),
+            invalidatesTags: ["WatchLater"]
         }),
 
         // remove video from watchlater 
@@ -56,6 +91,7 @@ export const plalylistApiSlice = apiSlice.injectEndpoints({
                 url: `${PLAYLISTS_URL}/remove/watchLater/${videoId}`,
                 method: "PATCH"
             }),
+            invalidatesTags: ["WatchLater"]
         }),
 
         // check is video already in watch later
@@ -72,6 +108,7 @@ export const plalylistApiSlice = apiSlice.injectEndpoints({
                 url: `${PLAYLISTS_URL}/u/watchLater/`,
                 method: "GET"
             }),
+            providesTags: ["WatchLater"]
         }),
     })
 })
@@ -79,6 +116,7 @@ export const plalylistApiSlice = apiSlice.injectEndpoints({
 
 export const {
     useGetUserPlaylistsQuery,
+    useGetPlaylistByIdQuery,
     useGetUserPlaylistsNameQuery,
     useLazyGetUserPlaylistsNameQuery,
     useCreatePlaylistMutation,
@@ -87,4 +125,7 @@ export const {
     useRemoveVideoFromWatchLaterMutation,
     useIsVideoAlreadyInWatchLaterQuery,
     useGetAllWatchLaterVideosQuery,
+    useRemoveVideoFromPlaylistMutation,
+    useUpdatePlaylistMutation,
+    useDeletePlaylistMutation,
 } = plalylistApiSlice
