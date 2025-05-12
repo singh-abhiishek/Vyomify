c3ba633d507e014407a46f0fd2561b8a7565fbd1
singh-abhiishek <8090singhabhi@gmail.com>
2025-05-12 12:37:13 +0530

    forgot to commit timely, later on i commit this all, LogoutBtn added

--- frontend/src/components/navbar/LogoutButton.jsx
+++ frontend/src/components/navbar/LogoutButton.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {useDispatch} from 'react-redux'
-import {logout as storeLogout} from '../../store/slice/authSlice.js'
+import {logout as storeLogout} from '../../store/slices/authSlice.js'
 import axios from 'axios'
 import Button from '../ui/button.jsx'
 
