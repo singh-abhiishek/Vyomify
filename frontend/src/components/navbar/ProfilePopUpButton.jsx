fb5e9ac4c8480db7fcdd5eedd5c1c13345ce48cc
singh-abhiishek <8090singhabhi@gmail.com>
2025-05-13 04:07:57 +0530

    btn added in navbar for profile and logout

--- frontend/src/components/navbar/ProfilePopUpButton.jsx
+++ frontend/src/components/navbar/ProfilePopUpButton.jsx
@@ -1,30 +1,38 @@
-import React, {useState} from 'react'
+import React, { useRef, useState } from 'react'
 import ProfilePopUp from './ProfilePopUp';
+import { useSelector } from "react-redux";
+import useOutsideClick from '../../hooks/UseOutsideClick';
 
 const ProfilePopUpButton = () => {
 
+  const data = useSelector(state => state.auth?.userData?.user)
   const [isOpen, setIsOpen] = useState(false);
-  const handleProfilePopUpData = (data) => {
-    setIsOpen(false)
-  }
+  // This hook listens for outside click, and close the pop up modal
+  const modalRef = useRef()
+  useOutsideClick(modalRef, () => setIsOpen(false), isOpen);
+
   return (
     <div className="relative">
-        <button
-            className="mr-1 md:mr-1.5 mt-1 justify-center rounded-3xl bg-white items-center h-8 w-8 sm:h-10 md:w-12 md:h-12 sm:w-10 cursor-pointer lg:h-12 lg:w-12"
-            aria-haspopup="true"
-            aria-expanded={isOpen}
-            onClick={() => setIsOpen(!isOpen)}
-        >
-            <img
-            className="w-8 h-8 sm:h-10 sm:w-10 md:w-12 md:h-12 rounded-full object-cover lg:h-12 lg:w-12"
-            src="https://media.istockphoto.com/id/2149922267/vector/user-icon.webp?a=1&b=1&s=612x612&w=0&k=20&c=eftd9nEYQYSWX_yZsHtkoo47x5l_Jp_2b-J4iD1pGPY="
-            width="32"
-            height="32"
-            alt="User"
-            />
-        </button>
+      <button
+        className="mr-1 md:mr-1.5 mt-1 justify-center rounded-3xl bg-white items-center h-8 w-8 sm:h-10 md:w-9 md:h-9 sm:w-10 cursor-pointer lg:h-12 lg:w-12"
+        aria-haspopup="true"
+        aria-expanded={isOpen}
+        onClick={() => setIsOpen(true)}
+      >
+        <img
+          className="w-8 h-8 sm:h-8 sm:w-8 md:w-9 md:h-9 rounded-full object-cover lg:h-12 lg:w-12"
+          // src='https://media.istockphoto.com/id/2149922267/vector/user-icon.webp?a=1&b=1&s=612x612&w=0&k=20&c=eftd9nEYQYSWX_yZsHtkoo47x5l_Jp_2b-J4iD1pGPY='
+          src={data?.avatar || `https://media.istockphoto.com/id/2149922267/vector/user-icon.webp?a=1&b=1&s=612x612&w=0&k=20&c=eftd9nEYQYSWX_yZsHtkoo47x5l_Jp_2b-J4iD1pGPY=`}
+          width="32"
+          height="32"
+          alt="User"
+        />
+      </button>
 
-        {isOpen && <ProfilePopUp sendDataToProfileButton = {handleProfilePopUpData}/>} 
+      {isOpen && <ProfilePopUp
+        modalRef={modalRef}
+        setIsOpen={setIsOpen}
+      />}
     </div>
   )
 }
