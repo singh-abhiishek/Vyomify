3590d1204dc62bbaf42806638758b85c8e9481fd
singh-abhiishek <8090singhabhi@gmail.com>
2025-05-17 00:13:24 +0530

    subscription page added

---
+++ frontend/src/pages/explore/Subscription/Subscription.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { useGetLatestVideosOfSubscribedChannelsQuery } from '../../../store/slices/subscriptionApiSlice'
+import { useSelector } from 'react-redux'
+import VideoGridItem from '../../../components/videoGrid/VideoGridItem'
+import { Spinner } from '../../../utils/loadingIndicator'
+
+const Subscription = () => {
+
+    const userData = useSelector(state => state?.auth?.userData)
+    const subscriberId = userData?.user?._id
+    // console.log("subscriberId from Subscription", subscriberId)
+
+    const { data: response, isLoading } = useGetLatestVideosOfSubscribedChannelsQuery(subscriberId)
+    const latestVideosList = response?.data
+    console.log("latestVideosList from Subscription", response)
+
+
+    if (isLoading) {
+        return <div className='bg-black text-white w-full flex flex-col items-center justify-center gap-2 py-10 text-center'>
+            <Spinner /> <p className="text-3xl text-gray-400">Loading</p>
+        </div>
+    }
+
+    return (
+        <div className="bg-black text-white w-full px-0">
+            {/* Heading */}
+            <div className="flex flex-col items-center mb-2">
+                <div className="flex items-end gap-0 sm:gap-2 text-white text-xl sm:text-2xl font-semibold">
+                    <h1 className="text-red-600 font-amaranth font-bold text-2xl sm:text-4xl leading-none">Latest Videos</h1>
+                    <p className="text-zinc-300 font-medium">from your Subscriptions</p>
+                </div>
+                <span className="block h-0.5 w-10 bg-gradient-to-r from-red-600 to-rose-500 mt-2 rounded-full" />
+            </div>
+
+            {/* Video Grid */}
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                {latestVideosList?.map((latestVideo, index) => (
+                    <div key={index}>
+                        <VideoGridItem {...latestVideo} />
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Subscription
