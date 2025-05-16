6beba701adb7a227d8e547a474faad7ce7093eca
singh-abhiishek <8090singhabhi@gmail.com>
2025-05-16 22:45:26 +0530

    home page created, late commit

---
+++ frontend/src/pages/explore/Home/Home.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react'
+import VideoGridItem from '../../../components/videoGrid/VideoGridItem'
+import { useSearchParams } from 'react-router-dom';
+import AllVideosScreen from './AllVideosScreen';
+import SearchedVideos from './SearchedVideos';
+
+const Home = () => {
+
+  const [searchParams] = useSearchParams();
+  const searchQuery = searchParams.get("query") || "";
+
+  useEffect(() => {
+    if (searchQuery) {
+      console.log("Search for:", searchQuery);
+    }
+  }, [searchQuery]);
+
+  return (
+    <div className='w-full h-full text-white bg-black '>
+      <div className="flex flex-wrap items-center justify-center gap-6">
+        {!searchQuery && <AllVideosScreen />}
+        {searchQuery && <SearchedVideos searchQuery={searchQuery} />}
+      </div>
+    </div>
+  )
+}
+
+export default Home
