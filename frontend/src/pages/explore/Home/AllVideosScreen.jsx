b4bfa7cbd5d82f1567d9a0fce8eead722e977bae
singh-abhiishek <8090singhabhi@gmail.com>
2025-05-16 22:46:05 +0530

    all video page added

---
+++ frontend/src/pages/explore/Home/AllVideosScreen.jsx
@@ -0,0 +1,45 @@
+import React, { useCallback, useEffect, useRef, useState } from 'react'
+import { useGetAllVideosQuery, useGetInfiniteAllVideosInfiniteQuery } from '../../../store/slices/videoApiSlice'
+import VideoGridItem from '../../../components/videoGrid/VideoGridItem'
+import { Spinner } from '../../../utils/loadingIndicator'
+
+const AllVideosScreen = () => {
+
+    const [page, setPage] = useState(1);
+
+    const { data: response, isLoading } = useGetAllVideosQuery({
+        sortBy: 'createdAt',
+        limit: 12,
+        page,
+        sortType: -1
+    })
+    const allVideos = response?.data?.allVideos
+
+
+    // scroll and pagination 
+    const a =
+        useGetInfiniteAllVideosInfiniteQuery({
+            sortBy: 'createdAt',
+            limit: 12,
+            page,
+            sortType: -1
+        })
+    console.log("a", a)
+
+
+    if (isLoading) {
+        return <div className='bg-black text-white w-full flex flex-col items-center justify-center gap-2 py-10 text-center'>
+            <Spinner /> <p className="text-3xl text-gray-400">Loading</p>
+        </div>
+    }
+
+    return (
+        <div className='flex flex-wrap gap-4 mt-0.5'>
+            {allVideos?.map((video, index) => (
+                <VideoGridItem key={index}{...video} />
+            ))}
+        </div>
+    )
+}
+
+export default AllVideosScreen
