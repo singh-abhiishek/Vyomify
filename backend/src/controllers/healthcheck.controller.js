1d9516e441a1c08719d7dc8c7171325eba556d51
singh-abhiishek <8090singhabhi@gmail.com>
2025-05-29 16:29:01 +0530

    change in health controller

--- backend/src/controllers/healthcheck.controller.js
+++ backend/src/controllers/healthcheck.controller.js
@@ -4,11 +4,10 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 
 const healthcheck = asyncHandler(async (req, res) => {
-    //TODO: build a healthcheck response that simply returns the OK status as json with a message
     return res
     .status(200)
     .json(
-        new ApiResponse(201, "Everything is OK")
+        new ApiResponse(200, "Everything is OK")
     )
 })
 
